804e14f43299583fca5cb0a7a6d2f2127d922c2b
rensawamo <106803080+rensawamo@users.noreply.github.com>
2023-12-29 19:34:32 +0900

    add timer ch

--- part1/main.go
+++ part1/main.go
@@ -7,12 +7,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 
 	// csvの読み込み
+	// flag.String("オプション名", "初期値", "説明")
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 300, "the time limit for the quiz in seconds")
+	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
@@ -26,18 +30,45 @@ func main() {
 
 	// problem[]
 	problems := parseLines(lines)
+	// 時間制限を追加する
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
-	correct := 0
+	correct := 0 // 正解数
+
+problemloop:
 	for i, p := range problems {
-		fmt.Printf("Problem %d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer) // 標準入力まち  入力がanswer に入る
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		ch := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scan("%s\n", &answer)
+			ch <- answer // 入力されたらchに流し込む
+		}()
+
+		select {
+		// タイムアップでchに流れこんだとき
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-ch:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+
+	// for i, p := range problems {
+	// 	fmt.Printf("Problem %d: %s = \n", i+1, p.q)
+	// 	var answer string
+	// 	fmt.Scanf("%s\n", &answer) // 標準入力まち  入力がanswer に入る
+	// 	if answer == p.a {
+	// 		correct++
+	// 	}
+	// }
+
+	// fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
