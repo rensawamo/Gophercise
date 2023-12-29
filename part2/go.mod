0280bc2375f5b6311efc2a7e19b1d94583941b51
rensawamo <106803080+rensawamo@users.noreply.github.com>
2023-12-29 20:25:52 +0900

    urlshort

---
+++ part2/go.mod
@@ -0,0 +1,8 @@
+module github.com/rensawamo/Gophercise
+
+go 1.21.5
+
+require (
+	github.com/gophercises/urlshort v0.0.0-20190723121003-cc800dbaf411
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ part2/main/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	urlshort "github.com/rensawamo/Gophercise"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "github.com/rensawamo/Gopherciseurlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlshort
+  url: github.com/rensawamo/Gophercise/urlshort
+- path: /urlshort-final
+  url: github.com/rensawamo/Gophercise/urlshort/tree/main
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
