b6215ed4a633169ac71845cdbae27d5c988eb5ef
rensawamo <ren0112ngzk@gmail.com>
2024-01-01 11:24:50 +0900

    コブラでCliを作成して実行した

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"strings"
-
 	"github.com/spf13/cobra"
 )
 
--- task/main.go
+++ task/main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/rensawamo/task/cmd"
 
 func main() {
+
+	// コブラを参照して  cmdでaddで  go run main.go add とかで 自作したcommandを  呼び込める		
 	cmd.RootCmd.Execute()
 }
