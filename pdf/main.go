b17f2376807a0a4400c22c52f437f225365a023b
rensawamo <ren0112ngzk@gmail.com>
2024-01-07 09:08:19 +0900

    readme 追加

--- pdf/main.go
+++ pdf/main.go
@@ -55,7 +55,7 @@ func main() {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Hello, world")
+	pdf.Cell(40, 10, "ドキュメントベースでいじってみる")
 	err := pdf.OutputFileAndClose("hello.pdf")
 	if err != nil {
 		panic(err)
